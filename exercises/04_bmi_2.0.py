342796cdd2f0c01d9a3dcfda72c4347b27545ad4
alba-molina-nyc <avmanyc@gmail.com>
2022-08-14 17:14:41 -0400

    bmi 2.0

--- exercises/04_bmi_2.0.py
+++ exercises/04_bmi_2.0.py
@@ -1,25 +1,25 @@
 def bmi_calculator():
-    height = input('please enter your height in m: ')
-    weight = input('please enter your weight in kg: ')
-    bmi = float(weight) / float(height) ** 2
+    height = float(input('please enter your height in m: '))
+    weight = float(input('please enter your weight in kg: '))
+    bmi = weight / height ** 2
     bmi_as_int = int(bmi)
     # print(f'your bmi is {bmi}')
     # print(f'your bmi as int is {bmi_as_int}')
 
-    if bmi_as_int <= 18:
-        print('you are underweight')
-    elif bmi_as_int > 18 and bmi_as_int < 25:
-        print('you have a normal weight')
-    elif bmi_as_int > 25 and bmi_as_int < 30:
-        print('you are overweight')
-    elif bmi_as_int > 30 and bmi_as_int < 35:
-        print('you are obese')
+    if bmi <= 18:
+        print(f'your bmi is {bmi} and you are underweight')
+    elif bmi > 18 and bmi_as_int < 25:
+        print(f'your bmi is {bmi} and you have a normal weight')
+    elif bmi > 25 and bmi_as_int < 30:
+        print(f'your bmi is {bmi} and you are overweight')
+    elif bmi > 30 and bmi_as_int < 35:
+        print(f'your bmi is {bmi} and you are obese')
     else:
-        print('you are clinically obese')
+        print(f'your bmi is {bmi_as_int} and you are clinically obese')
 
 
 if __name__ == '__main__':
-    print(bmi_calculator())
+    bmi_calculator()
 
 
     # please enter your weight in kg: 66.6781
